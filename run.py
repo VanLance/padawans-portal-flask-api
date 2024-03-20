8628e5cab2f011b16fe90f807e2cd6923dbde92b
VanLance <dylan23smith1990@gmail.com>
2024-03-19 19:02:19 -0500

    add post crud routes

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 
+from uuid import uuid4
+
 app = Flask(__name__)
 
 users = {
@@ -16,12 +18,12 @@ users = {
 }
 
 posts = {
-    1 : {
+    '1' : {
         'author' : 2,
         'title' : 'Welcome to Flask',
         'body' : 'Welcome to our first full-stack app, it\'s SUPER easy and simple!'
     },
-    2 : {
+    '2' : {
         'author' : 1,
         'title' : 'Flask intro',
         'body' : 'I would\'nt say it is THAT simple'
@@ -85,3 +87,46 @@ def del_user():
     return {
         'err' : "can't delete that user they aren't there. . . "
     }
+
+# POST ROUTES
+
+@app.post('/post')
+def create_post():
+    post_data = request.get_json()
+    if post_data['author'] not in users:
+        return {"message": "user does not exist"}, 400
+    post_id = uuid4().hex
+    posts[post_id] = post_data
+    return {
+        'message': "Post created",
+        'post-id': post_id
+        }, 201
+
+@app.get('/post')
+def get_posts():
+    try:
+        return list(posts.values()), 200
+    except:
+        return {'message':"Failed to get posts"}, 400
+
+@app.get('/post/<post_id>')
+def get_ind_post(post_id):
+    try: 
+        return posts[post_id], 200
+    except KeyError:
+        return {'message':"invalid post"}, 400
+
+@app.put('/post')
+def update_post():
+    pass
+
+@app.delete('/post')
+def delete_post():
+    post_data = request.get_json()
+    post_id = post_data['id']
+
+    if post_id not in posts:
+        return { 'message' : "Invalid Post"}, 400
+    
+    posts.pop(post_id)
+    return {'message': f'Post: {post_id} deleted'}
